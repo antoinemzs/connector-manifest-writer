ec93718d69b8d5e648a308767ddc7a2d0f052996
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-11-27 18:01:18 +0100

    very crude POC
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- connector-manifest-writer/__main__.py
+++ connector-manifest-writer/__main__.py
@@ -1,5 +1,9 @@
 import argparse
-
+import toml
+import subprocess
+import json
+import base64
+import ast
 
 def main():
     parser = argparse.ArgumentParser(
@@ -10,7 +14,34 @@ def main():
     parser.add_argument("target", action='extend', nargs='+', default=[])
     args = parser.parse_args()
 
-    print(args)
+    contracts = []
+    for dir in args.target:
+        pyproject_path = "{}/pyproject.toml".format(dir)
+        pyproject_contents = toml.load(pyproject_path)
+
+        tool_cmw = pyproject_contents.get("tool").get("cmw")
+
+        process = subprocess.run(tool_cmw.get("config-dump-command").split(), text=True, capture_output=True, cwd=dir)
+        config_schema_str = process.stdout
+
+        logo_str = None
+        with(open("{}/{}".format(dir, tool_cmw.get("icon-path")), "rb") as f):
+            bytes = f.read()
+            logo_str = "data:image/png;base64,{}".format(base64.encodebytes(bytes))
+
+        contract = {}
+        contract["slug"] = tool_cmw.get("slug")
+        contract["use_cases"] = tool_cmw.get("use_cases")
+        contract["logo"] = logo_str
+        contract["config_schema"] = ast.literal_eval(config_schema_str)
+
+        contracts.append(contract)
+
+    manifest = {"id": "filigran-catalog-id", "name": "OpenCTI Connectors contracts", "description": "",
+                "version": "rolling", "contracts": contracts}
+
+    print(json.dumps(manifest))
+
 
 if __name__ == "__main__":
     main()
