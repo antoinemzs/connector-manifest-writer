98fb7058515be9d60419187c1f8ff2a7300b08f0
Camille Roux <camille.roux@filigran.io>
2025-12-19 16:25:27 +0100

    [generator] Add some field

--- connector-manifest-writer/__main__.py
+++ connector-manifest-writer/__main__.py
@@ -24,24 +24,27 @@ def main():
         install_process = subprocess.run(tool_cmw.get("install-command").split(), text=True, capture_output=True, cwd=dir)
         out = install_process.stdout
 
-
         process = subprocess.run(tool_cmw.get("config-dump-command").split(), text=True, capture_output=True, cwd=dir)
         config_schema_str = process.stdout
 
-        logo_str = None
-        with(open("{}/{}".format(dir, tool_cmw.get("icon-path")), "rb") as f):
-            bytes = f.read()
-            logo_str = "data:image/png;base64,{}".format(base64.encodebytes(bytes))
+        with open(f"{dir}/{tool_cmw.get('icon-path')}", "rb") as f:
+            raw_bytes = f.read()
+        logo_b64 = base64.b64encode(raw_bytes).decode("utf-8")
+        logo_str = f"data:image/png;base64,{logo_b64}"
 
         contract = {}
+        contract["title"] = tool_cmw.get("title")
         contract["slug"] = tool_cmw.get("slug")
+        contract["short_description"] = tool_cmw.get("short_description", "")
         contract["use_cases"] = tool_cmw.get("use_cases")
         contract["logo"] = logo_str
+        contract["verified"] = True
+        contract["container_type"] = tool_cmw.get("container_type")
         contract["config_schema"] = ast.literal_eval(config_schema_str)
 
         contracts.append(contract)
 
-    manifest = {"id": "filigran-catalog-id", "name": "OpenCTI Connectors contracts", "description": "",
+    manifest = {"id": "filigran-catalog-id", "name": "OpenAEV Connectors contracts", "description": "",
                 "version": "rolling", "contracts": contracts}
 
     print(json.dumps(manifest, indent=4))
