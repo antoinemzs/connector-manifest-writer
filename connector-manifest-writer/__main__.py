0f3876aac0179476e6dfabb83be05196d4651fc2
Camille Roux <camille.roux@filigran.io>
2025-12-30 14:56:44 +0100

    Get the manifest metadata json

--- connector-manifest-writer/__main__.py
+++ connector-manifest-writer/__main__.py
@@ -19,10 +19,13 @@ def main():
         pyproject_path = "{}/pyproject.toml".format(dir)
         pyproject_contents = toml.load(pyproject_path)
 
+        static_metadata_path = "{}/manifest-metadata.json".format(dir)
+        with open(static_metadata_path, "r") as json_data_file:
+            static_metadata_contents = json.load(json_data_file)
+
         tool_cmw = pyproject_contents.get("tool").get("cmw")
 
-        install_process = subprocess.run(tool_cmw.get("install-command").split(), text=True, capture_output=True, cwd=dir)
-        out = install_process.stdout
+        subprocess.run(tool_cmw.get("install-command").split(), text=True, capture_output=True, cwd=dir)
 
         process = subprocess.run(tool_cmw.get("config-dump-command").split(), text=True, capture_output=True, cwd=dir)
         config_schema_str = process.stdout
@@ -32,23 +35,15 @@ def main():
         logo_b64 = base64.b64encode(raw_bytes).decode("utf-8")
         logo_str = f"data:image/png;base64,{logo_b64}"
 
-        contract = {}
-        contract["title"] = tool_cmw.get("title")
-        contract["slug"] = tool_cmw.get("slug")
-        contract["short_description"] = tool_cmw.get("short_description", "")
-        contract["use_cases"] = tool_cmw.get("use_cases")
-        contract["logo"] = logo_str
-        contract["verified"] = True
-        contract["container_type"] = tool_cmw.get("container_type")
-        contract["config_schema"] = ast.literal_eval(config_schema_str)
+        static_metadata_contents["logo"] = logo_str
+        static_metadata_contents["config_schema"] = ast.literal_eval(config_schema_str)
 
-        contracts.append(contract)
+        contracts.append(static_metadata_contents)
 
     manifest = {"id": "filigran-catalog-id", "name": "OpenAEV Connectors contracts", "description": "",
                 "version": "rolling", "contracts": contracts}
 
     print(json.dumps(manifest, indent=4))
-
 
 if __name__ == "__main__":
     main()
