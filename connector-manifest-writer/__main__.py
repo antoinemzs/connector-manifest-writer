6f1437f042329c87850cdaa7850c0b8867080a58
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-01-29 14:28:15 +0100

    add exception wrapper to explain source of error
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- connector-manifest-writer/__main__.py
+++ connector-manifest-writer/__main__.py
@@ -16,29 +16,32 @@ def main():
 
     contracts = []
     for dir in args.target:
-        pyproject_path = "{}/pyproject.toml".format(dir)
-        pyproject_contents = toml.load(pyproject_path)
+        try:
+            pyproject_path = "{}/pyproject.toml".format(dir)
+            pyproject_contents = toml.load(pyproject_path)
 
-        static_metadata_path = "{}/manifest-metadata.json".format(dir)
-        with open(static_metadata_path, "r") as json_data_file:
-            static_metadata_contents = json.load(json_data_file)
+            static_metadata_path = "{}/manifest-metadata.json".format(dir)
+            with open(static_metadata_path, "r") as json_data_file:
+                static_metadata_contents = json.load(json_data_file)
 
-        tool_cmw = pyproject_contents.get("tool").get("cmw")
+            tool_cmw = pyproject_contents.get("tool").get("cmw")
 
-        subprocess.run(tool_cmw.get("install-command").split(), text=True, capture_output=True, cwd=dir)
+            subprocess.run(tool_cmw.get("install-command").split(), text=True, capture_output=True, cwd=dir)
 
-        process = subprocess.run(tool_cmw.get("config-dump-command").split(), text=True, capture_output=True, cwd=dir)
-        config_schema_str = process.stdout
+            process = subprocess.run(tool_cmw.get("config-dump-command").split(), text=True, capture_output=True, cwd=dir)
+            config_schema_str = process.stdout
 
-        with open(f"{dir}/{tool_cmw.get('icon-path')}", "rb") as f:
-            raw_bytes = f.read()
-        logo_b64 = base64.b64encode(raw_bytes).decode("utf-8")
-        logo_str = f"data:image/png;base64,{logo_b64}"
+            with open(f"{dir}/{tool_cmw.get('icon-path')}", "rb") as f:
+                raw_bytes = f.read()
+            logo_b64 = base64.b64encode(raw_bytes).decode("utf-8")
+            logo_str = f"data:image/png;base64,{logo_b64}"
 
-        static_metadata_contents["logo"] = logo_str
-        static_metadata_contents["config_schema"] = ast.literal_eval(config_schema_str)
+            static_metadata_contents["logo"] = logo_str
+            static_metadata_contents["config_schema"] = ast.literal_eval(config_schema_str)
 
-        contracts.append(static_metadata_contents)
+            contracts.append(static_metadata_contents)
+        except Exception as e:
+            raise RuntimeError(f"There was a problem processing directory '{dir}'") from e
 
     manifest = {"id": "filigran-catalog-id", "name": "OpenAEV Connectors contracts", "description": "",
                 "version": "rolling", "contracts": contracts}
